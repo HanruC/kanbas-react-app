f5d60c1afbbddf7f4c30873b2218a250bc8a45c8
HanruC <chen.hanru@northeastern.edu>
2024-02-21 15:29:56 -0500

    lab finished

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import Labs from "./Labs";
+import Kanbas from "./Kanbas";
+import HelloWorld from "./Labs/a3/HelloWorld";
+import { HashRouter, Link, Navigate, Route, Routes } from "react-router-dom";
+
+function App() {
+  const screen = "Labs";
+  return (
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Kanbas" />} />
+          <Route path="/Hello" element={<HelloWorld />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
+}
+
+export default App;
