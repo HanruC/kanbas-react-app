19c47f465735c981c268f6183b8390080a007695
HanruC <chen.hanru@northeastern.edu>
2024-02-21 22:37:40 -0500

    3.3 dashboard done

---
+++ src/Kanbas/Dashboard/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import courses from '../Database/courses.json';
+import { Link } from "react-router-dom";
+import './index.css';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+
+// Function to dynamically assign a background color
+// function getBackgroundColor(courseId) {
+//     const colors = ['#61dafb', '#6f42c1', '#3776ab', '#f0db4f', '#5382a1'];
+//     return colors[parseInt(courseId, 10) % colors.length];
+// }
+
+function Dashboard() { 
+    return (
+        <div className="p-4">
+            <h1>Dashboard</h1>
+            <hr />
+            <h2>Published Courses ({courses.length})</h2>
+            <hr />
+            <div className="row row-cols-1 row-cols-md-5 g-4">
+                {courses.map(course => (
+                    <div key={course._id} className="col" style={{ width: 300 }}>
+                        <div className="card">
+                            <div className="card-image">
+                                <BsThreeDotsVertical className="card-icon" style={{ color: 'gray' }} />
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">{course.name}</h5>
+                                <p className="card-text">{course.number}</p>
+                                <p className="card-text">
+                                    {course.startDate} - {course.endDate}
+                                </p>
+                                <Link to={`/Kanbas/Courses/${course._id}`} className="btn btn-primary">
+                                    View Course
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,8 +1,23 @@
+import KanbasNavigation from "./KanbasNavigation";
+import { Route, Routes, Navigate } from "react-router";
+//import Courses from "./Courses";
+//import Account from "./Account";
+import Dashboard from "./Dashboard";
+
 function Kanbas() {
-    return(
-       <div>
-          <h1>Kanbas</h1>
-       </div>
-    );
- }
- export default Kanbas
+  return (
+    <div className="d-flex">
+      <KanbasNavigation />
+      <div>
+        <Routes>
+
+          {/* <Route path="Account" element={<Account />} /> */}
+          <Route path="Dashboard" element={<Dashboard />} />
+          {/* <Route path="Courses/:courseId/*" element={<Courses />} /> */}
+          <Route path="Calendar" element={<h1>Calendar</h1>} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+export default Kanbas;
